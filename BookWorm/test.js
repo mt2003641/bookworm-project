4911ca9deb463ad74fb4c1f38744f0742ce9e5fd
mt2003641 <mt2003641@qu.edu.qa>
2026-05-02 20:14:09 +0300

    v

--- BookWorm/test.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function run() {
-  const totalPosts = await prisma.post.count();
-  console.log("Total Posts:", totalPosts);
-}
-
-run();
